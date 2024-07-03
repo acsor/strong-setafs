7cd80edc1dac5b544f5f3cb51a368ad3e0c76503
Acsor <nildexo@yandex.com>
2024-07-03 15:32:07 +0200

    Add first stub of setaf.py

---
+++ src/setaf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+
+from parser.DisplayGraphCommand import DisplayGraphCommand
+from parser.wrap_command import wrap_command
+
+
+def _build_parser():
+    parser = argparse.ArgumentParser(
+        description="Collection of SETAF utilities"
+    )
+    subparsers = parser.add_subparsers(
+        dest='subcommand', required=True
+    )
+    commands = (
+        DisplayGraphCommand,
+    )
+
+    for c in commands:
+        p = c.set_up_parser(
+            subparsers.add_parser(
+                c.NAME,
+                description=c.DESCRIPTION
+            )
+        )
+        p.set_defaults(
+            func=wrap_command(c)
+        )
+
+    return parser
+
+
+def main():
+    parser = _build_parser()
+    args = parser.parse_args()
+
+    return args.func(args)
+
+
+if __name__ == "__main__":
+    exit(main())
