d10cca80777460883dc148b376af6ebd90cbf631
Acsor <nildexo@yandex.com>
2024-07-08 21:07:50 +0200

    Tweak SETAFReader._call_aux() to take a file-type argument rather than a path-like one

--- src/SETAFReader.py
+++ src/SETAFReader.py
@@ -6,29 +6,28 @@ class SETAFReader:
     def __init__(self):
         pass
 
-    def __call__(self, path):
+    def __call__(self, file):
         return SETAF(
-            *self._call_aux(path)
+            self._call_aux(file)
         )
 
     @staticmethod
-    def _call_aux(path):
+    def _call_aux(file):
         line_count = 0
 
-        with open(path, "r") as file:
-            for raw_line in file:
-                line = tuple(
-                    map(
-                        int, raw_line.split(" ")
-                    )
+        for raw_line in file:
+            line = tuple(
+                map(
+                    int, raw_line.split(" ")
                 )
+            )
 
-                if line_count > 0:
-                    if line[-1] != 0:
-                        raise ValueError(
-                            "A 0 is expected at the end of the line"
-                        )
+            if line_count > 0:
+                if line[-1] != 0:
+                    raise ValueError(
+                        "A 0 is expected at the end of the line"
+                    )
 
-                yield Attack(line[0:-2], line[-2])
+            yield Attack(line[0:-2], line[-2])
 
-                line_count += 1
+            line_count += 1
