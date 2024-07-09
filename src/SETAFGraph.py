ba3369b66fcfcce80e4e7d1eb77daf0fd1594bb5
Acsor <nildexo@yandex.com>
2024-07-09 18:05:05 +0200

    Add SETAFGraph.node_colors() method

--- src/SETAFGraph.py
+++ src/SETAFGraph.py
@@ -1,5 +1,8 @@
 import networkx
 
+from src.SETAFLabeling import Label
+
+
 class SETAFGraph(networkx.DiGraph):
     def __init__(self, setaf):
         super().__init__()
@@ -7,15 +10,32 @@ class SETAFGraph(networkx.DiGraph):
         self._display_labels = dict()
 
         for a in setaf.attacks:
-            self.add_node(a.attackers)
+            self.add_node(a)
             self.add_node(a.attacked)
 
-            self._display_labels[a.attackers] = "{%s}" % ",".join(
+            self._display_labels[a] = "{%s}" % ",".join(
                 map(str, a.attackers)
             )
             self._display_labels[a.attacked] = str(a.attacked)
 
-            self.add_edge(a.attackers, a.attacked)
+            self.add_edge(a, a.attacked)
+
+    @property
+    def setaf(self):
+        return self._setaf
+
+    def node_colors(self, labeling):
+        colors = {
+            Label.IN: '#008000',
+            Label.OUT: '#FF0000',
+            Label.UNDEC: '#A9A9A9'
+        }
+
+        return tuple(
+            map(
+                lambda n: colors[labeling[n]], self.nodes
+            )
+        )
 
     @property
     def display_labels(self):
